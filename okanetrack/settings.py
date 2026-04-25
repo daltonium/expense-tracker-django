09566d70667850659bac77208c8718925660b133
daltonium <dalton.mdj@gmail.com>
2026-04-25 13:35:29 +0530

    Debugged V3

--- okanetrack/settings.py
+++ okanetrack/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = 'django-insecure-mrxh$86dp&98i-e%-&5!!8@^e2$myx&-!tg%&s7_s0r*)nyxn4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
